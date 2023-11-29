bd211f2439354706f391b068a5a7cf5143c700fb
PondD <46706534+Nontagorn@users.noreply.github.com>
2023-11-29 22:45:01 +0700

    Add files via upload

---
+++ src/src/Main.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Main {
+    private  You u ;
+    private Enemy n ;
+    private Shield sh ;
+    private Sword sw ;
+
+    public static void main(String[] args){
+        You u = new You();
+        Enemy n = new Enemy();
+        Shield sh = new Shield();
+        Sword sw = new Sword();
+
+
+    }
+}
---
+++ src/src/Sword.java
@@ -0,0 +1,7 @@
+public class Sword {
+    int atk;
+    public Sword(){
+        this.atk = atk;
+        atk = 15;
+    }
+}
